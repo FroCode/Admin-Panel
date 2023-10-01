cf19f644c6268167d3c74690634a65ec458efa2c
Haddouche Maamar <frocodeof@gmail.com>
2023-10-01 22:26:45 +0200

    Add Login Page

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/home/Home.jsx';
+import Login from './pages/login/Login.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> This is From FroCode Developer.
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+        <Route index element={<Home/>}/>
+        <Route path='login' element={<Login/>}/>
+        
+      </Routes>
+      
+      </BrowserRouter>
+      
     </div>
   );
 }
