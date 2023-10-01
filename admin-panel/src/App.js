22dfff21fd89b5e0b9aebed16c694cb9103472ed
Haddouche Maamar <frocodeof@gmail.com>
2023-10-01 23:45:16 +0200

    Add Routes

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,5 +1,8 @@
 import Home from './pages/home/Home.jsx';
 import Login from './pages/login/Login.jsx';
+import List from './pages/list/List.jsx';
+import Single from './pages/single/Single.jsx';
+import New from './pages/new/New.jsx';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -17,7 +20,11 @@ function App() {
         <Route path="/" />
         <Route index element={<Home/>}/>
         <Route path='login' element={<Login/>}/>
-        
+        <Route path="user">
+          <Route index element={<List/>} />
+          <Route path=":userId" element={<Single/>} />
+          <Route path=":userId" element={<New/>} />
+        </Route>
       </Routes>
       
       </BrowserRouter>
