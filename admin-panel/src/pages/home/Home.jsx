7ba630c93b689899ba51381707068afee9a3343e
Haddouche Maamar <frocodeof@gmail.com>
2023-10-02 22:04:05 +0200

    Styling Navbar

--- admin-panel/src/pages/home/Home.jsx
+++ admin-panel/src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './home.scss'
 import SideBar from '../../components/sidebar/SideBar'
+import Navbar from '../../components/navbar/Navbar'
 function Home() {
   return (
     <div className='home'>
       <SideBar />
       <div className="homeContainer">
-        Con
+        <Navbar/>
       </div>
     </div>
   )
