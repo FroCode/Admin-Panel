16767a065143c1d1e885236356ee7575d04451aa
Haddouche Maamar <frocodeof@gmail.com>
2023-10-04 23:17:05 +0200

    Update

--- admin-panel/src/pages/home/Home.jsx
+++ admin-panel/src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './home.scss'
 import SideBar from '../../components/sidebar/SideBar'
 import Navbar from '../../components/navbar/Navbar'
+import Widgets from '../../components/widgets/Widgets'
 function Home() {
   return (
     <div className='home'>
       <SideBar />
       <div className="homeContainer">
         <Navbar/>
+        <div className="widgets">
+          <Widgets/>
+        </div>
       </div>
     </div>
   )
